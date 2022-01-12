920d578be60cacc3d5de0e8852d6f6d14a5b3be0
Zack Gaddy <zack.e.gaddy@gmail.com>
2022-01-12 06:54:19 -0700

    finish Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Zack Gaddy ";
+    int age =  23;
+    String state = " Colorado";
+    System.out.println(name + age + state);
+    
+  }
+}
