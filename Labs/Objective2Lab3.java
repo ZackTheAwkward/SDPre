27a84febe47a5827167e43fe1001909caa5baace
Zack Gaddy <zack.e.gaddy@gmail.com>
2022-01-12 20:31:58 -0700

    finish Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+  public static void main(String[] args)  {
+    double num1, num2, num3, average;
+   num1 = 17;
+   num2 = 45;
+   num3 = 92;
+   average = (num1 + num2 + num3)/3;
+    System.out.println(average);
+
+  }
+}
