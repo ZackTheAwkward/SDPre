8e66a6830caf1664982b4619c6344e15f945351a
Zack Gaddy <zack.e.gaddy@gmail.com>
2022-01-13 07:14:40 -0700

    finish Objective3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,10 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    double side1, side2, hypotenuse;
+    side1=25;
+    side2=60;
+    hypotenuse= Math.sqrt((side1*side1)+(side2*side2));
+    System.out.println(hypotenuse);
+
+  }
+}
