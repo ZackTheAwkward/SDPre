57f5f85a1f388e9de6a7188b6bb1b1234d2829ff
Zack Gaddy <zack.e.gaddy@gmail.com>
2022-01-28 20:07:35 -0700

    finish Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,12 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+
+    for (int i=1; i<=10; i+=1) {
+      System.out.println(i);
+    }
+    for (int k=10; k>=1; k-=1) {
+      System.out.println(k);
+    }
+
+  }
+}
