94106872753176765ca5f3eee4f7f499b744c96f
Zack Gaddy <zack.e.gaddy@gmail.com>
2022-02-02 11:01:07 -0700

    finish Objective9Lab4

--- Labs/Objective9Lab4.java
+++ Labs/Objective9Lab4.java
@@ -11,7 +11,7 @@ public class Objective9Lab4 {
                 num2 = kb.nextDouble();
 
     double average = findAverage(num1, num2);
-    System.out.println("The average of " + num1 + " and " + num2 + " is " + average);
+    System.out.println("The average of " + num1 + " and " + num2 + " is: " + average);
 
     kb.close();
 
